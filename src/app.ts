4e65511ab0641c7e3d58c6e2e12ac004ce573ebd
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-01-30 10:34:24 +0530

    new chenges added jasim

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import cors from "cors";
-import super_admin_route from "./routes/super_admin_route";
-import admin_route from "./routes/admin_route";
-import hr_route from "./routes/hr_routes";
+import express from "express"
+import cors from "cors"
+import super_admin_route from "./routes/super_admin_route"
+import admin_route from './routes/admin_route'
+import hr_route from './routes/hr_routes'
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use("/api/super_admin",super_admin_route)
+app.use("/api/admin",admin_route)
+app.use('/api/hr',hr_route)
 
-app.use("/api/superadmin", super_admin_route);
+export default app
 
-app.use("/api/admin", admin_route);
-app.use("/api/hr", hr_route);
 
-export default app;
