37f27c1eac2538900fba00c4eb66860a352eac46
MohammedJasim001 <jasimat001@gmail.com>
2025-01-31 15:28:22 +0530

    delete sql related contents

--- dist/entities/tenants_entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { Entity, PrimaryGeneratedColumn,Column } from "typeorm";
-// @Entity('Tenants')
-// export class Tenant{
-//     @PrimaryGeneratedColumn()
-//     id!:number;
-//     @Column({type:"varchar",length:100,unique:true})
-//     tenant_name!:string
-// }
